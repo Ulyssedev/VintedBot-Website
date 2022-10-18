06d9f5af75633862a62e4b41ca5e654409f14880
LowHigh <lowhigh.ytb@gmail.com>
2022-10-18 23:22:48 +0200

    Basic user auth

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, connectAuthEmulator, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKd8gi7RIjeEIVj9GnAn4AvkBxgEmCJ2w",
+    authDomain: "vintedbot-3f0d6.firebaseapp.com",
+    projectId: "vintedbot-3f0d6",
+    storageBucket: "vintedbot-3f0d6.appspot.com",
+    messagingSenderId: "495357704111",
+    appId: "1:495357704111:web:df56b60220ec07fd97e15a",
+    measurementId: "G-F70EHN3FVM"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth();
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+
+
+
+const signupForm = document.querySelector('.signup')
+if (signupForm) {
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(cred => {
+        console.log('user created:', cred.user)
+        signupForm.reset()
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  })
+}
+
+const logoutButton = document.querySelector('.logout')
+if (logoutButton) {
+  logoutButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    signOut(auth).then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  })
+}
+
+const loginForm = document.querySelector('.login')
+if (loginForm) {
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+}
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    console.log('user logged in:', user)
+    document.querySelector('.logout').style.display = 'inline-block'
+  } else {
+    console.log('user logged out')
+    document.querySelector('.button.small').style.display = 'inline-block'
+  }
+})
