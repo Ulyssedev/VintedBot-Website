e163d215ce640ad49ebb22c06edb570d207a939b
Ulyssedev <lowhigh.ytb@gmail.com>
2023-07-08 02:54:35 +0200

    ✨ Pricing page rewrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
       watch: {
-        ignored: ["**.log"],
-      },
-    },
+        ignored: ["**.log"]
+      }
+    }
   },
+  integrations: [tailwind()]
 });
