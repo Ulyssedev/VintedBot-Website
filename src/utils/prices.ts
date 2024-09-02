d76e9a1106e7de54626fbe1fe02199aa8d02026b
Ulysse <71192394+Ulyssedev@users.noreply.github.com>
2024-09-02 18:03:31 +0200

    🎨 Add ultra plan on pricing page (#24)
    * 🎨 Add ultra plan on pricing page
    
    * ➕ Update prices
    
    * ✨ Add footer in pricing
    
    * 🎨  Finalize ultra price page
    
    * 🎨 Finalize ultra page

--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -1,17 +1,17 @@
 let prices = {
   starter: "",
-  classic: "",
   pro: "",
+  ultra: "",
 };
 
 if (process.env.NODE_ENV === "development") {
-  prices.starter = "price_1KnnTeGIWjSo9YB2SmEvdgv6";
-  prices.classic = "price_1MVeROGIWjSo9YB2InixG6Yr";
+  prices.starter = "price_1MVeROGIWjSo9YB2InixG6Yr";
   prices.pro = "price_1MVeRqGIWjSo9YB2uDUR9nDF";
+  prices.ultra = "price_1Ps5zFGIWjSo9YB270E7pNME";
 } else {
-  prices.starter = "price_1Knr4IGIWjSo9YB2RXmxY6gT";
-  prices.classic = "price_1KqHCPGIWjSo9YB2neXZUfTd";
+  prices.starter = "price_1KqHCPGIWjSo9YB2neXZUfTd";
   prices.pro = "price_1KqHDOGIWjSo9YB2WJVE39mF";
+  prices.ultra = "price_1Ps64nGIWjSo9YB2E1Wtv1Ms";
 }
 
 export default prices;
